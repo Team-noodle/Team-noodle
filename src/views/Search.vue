b9f3b3a2f8c1e0795e7f5b076a8d27f05498cce2
unknown <0901KentaKii>
2021-03-12 19:41:11 +0900

    search prduction

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,11 +1,42 @@
 <template>
- <div></div> 
+  <div>
+    検索
+    <div id="v-for">
+    <h3>v-for</h3>
+    <div v-for="cooking recipie">{{recipe}}</div>    
+  </div>
+  <button @click="cooking">検索</button>
+</div>  
 </template>
+
 <script>
 export default {
-  
+  data() {
+    return {
+      recipe: [
+        {
+          categoryname:"ハンバーグ"
+          url:""
+        }
+        {
+          categoryname:"パスタ"
+          url:"" 
+        }
+         {
+          categoryname:"かつ丼"
+          url:"" 
+        } {
+          categoryname:"ポテトサラダ"
+          url:"" 
+        } {
+          categoryname:"肉じゃが"
+          url:"" 
+        }
+      ]
+    }
+  }
 }
-</script>
-<style>
+    
 
-</style>
+</script>
+<style></style>
